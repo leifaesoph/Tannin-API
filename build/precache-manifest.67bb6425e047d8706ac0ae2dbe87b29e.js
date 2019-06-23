0e90c1c45cd14e2e83a869ef5e11d5467df21605
Dennis <dennissarmiento808@gmail.com>
2019-06-23 09:23:41 +0800

    up

---
+++ build/precache-manifest.67bb6425e047d8706ac0ae2dbe87b29e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "303c761c7ff9d2acf2c0",
+    "url": "/Tannin/static/css/main.49ef82f8.chunk.css"
+  },
+  {
+    "revision": "303c761c7ff9d2acf2c0",
+    "url": "/Tannin/static/js/main.167ae215.chunk.js"
+  },
+  {
+    "revision": "dafb2ad3982900ac4088",
+    "url": "/Tannin/static/js/runtime~main.b71e23b5.js"
+  },
+  {
+    "revision": "f19fa847ed75ea3876d4",
+    "url": "/Tannin/static/js/2.43b80c95.chunk.js"
+  },
+  {
+    "revision": "3f6e7d63781595311f7b94a83c96f5a1",
+    "url": "/Tannin/index.html"
+  }
+];

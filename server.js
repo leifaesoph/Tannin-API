6fca98408acf36414471ede80454438100927b8b
Turbocake64 <micahrabinowitz@gmail.com>
2019-06-29 16:10:55 -0700

    Mongoose update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 var mailer = require("nodemailer");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wines"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://<MLRabinowitz>:<MLRpassword123>@ds245387.mlab.com:45387/heroku_lj97cfjz"
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
@@ -18,14 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors());
-// console.log(db);
-// app.use(
-//     session({
-//         secret: process.env.APP_SECRET || 'this is the default passphrase',
-//         store: new MongoStore({ mongooseConnection: db }),
-//         resave: false,
-//         saveUninitialized: false
-//     }))
 app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true }))
 app.use(passport.initialize());
 app.use(passport.session());
@@ -38,31 +30,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, '../build/'));
     })
 }
-// app.get('/',(req,res)=>(
-//     db.MasterWineList.find({}).then(Wines=>{
-//         res.json(Wines)
-//     })
-// ))
-
-// app.post('/',(req,res)=>{
-//     console.log("req.body:", req.body)
-//     return (
-//     db.MasterWineList.create({
-//         name: req.body.name,
-//         primaryFlavors: req.body.primaryFlavors.split(','),
-//         region: req.body.region
-//     }).then(Wines=>
-//         res.json(Wines)
-//     )
-// )})
-
-// app.delete('/:_id',(req,res)=>{
-//     db.MasterWineList.findById(req.params._id)
-//       .then(dbWine => dbWine.remove())
-//       .then(dbWine => res.json(dbWine))
-//       //if error, catch it and give a 422 response as well as logging the error in the console
-//       .catch(err => res.status(422).json(err));
-// })
 
 app.use(routes);
 // app.use(function (err, req, res, next) {
